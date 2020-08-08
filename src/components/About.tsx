b51f0b9f361b3f50626269330defa2ba8b3e2163
Matija Turčec <turcec.matija@gmail.com>
2020-08-08 23:59:36 +0200

    updated about comp

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
 
 function About() {
   return (
-    <section>About</section>
+    <div>
+      <h2>
+        About Us
+      </h2>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam nobis repellat quae laborum voluptas nisi id? Maxime aut placeat quia rerum! Dolorum quia corrupti id corporis, esse accusamus quisquam porro?Illum ut iusto dolorem nesciunt ad at voluptatem quisquam maxime? Tenetur quam veniam laboriosam doloremque! Obcaecati dicta magnam doloremque ullam, in numquam unde fugit vel temporibus ipsum deleniti optio soluta?
+      </p>
+      <Button color="secondary">
+        Learn more
+      </Button>
+    </div>
   );
 }
 
