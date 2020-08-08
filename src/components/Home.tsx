152f6d0e0a2c5bd037d2963d1b1d9f440fdae3a8
Matija Turčec <turcec.matija@gmail.com>
2020-08-08 22:33:15 +0200

    basic structure for home page component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,29 @@
 import React from 'react';
+import Banner from './components-small/Banner';
 
 function Home() {
   return (
-    <section>Home</section>
+    <div>
+      <Banner />
+      <section>
+        ABOUT
+      </section>
+      <section>
+        WHAT WE DO
+      </section>
+      <section>
+        Portfolio
+      </section>
+      <section>
+        Impressions
+      </section>
+      <section>
+        Blog?
+      </section>
+      <section>
+        Portfolio
+      </section>
+    </div>
   );
 }
 
