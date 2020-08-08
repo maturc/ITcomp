5bdf0b6ae140f163b1e81d9eb0e6059bde42ccb7
Matija Turčec <turcec.matija@gmail.com>
2020-08-08 23:59:24 +0200

    added more props to learnmore

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,4 +1,11 @@
 export type INav = {
-  classProp: string | undefined
-  setPlayDrawerAnimation?: undefined | React.Dispatch<React.SetStateAction<boolean>>
+  classProp: string | undefined;
+  setPlayDrawerAnimation?: undefined | React.Dispatch<React.SetStateAction<boolean>>;
+}
+export type ILearnMore = {
+  header: string;
+  paragraph: string;
+  button: string;
+  src: string;
+  alt?: string | undefined;
 }
