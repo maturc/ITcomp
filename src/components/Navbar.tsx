289585c0ba996663e1c39a380ec82f4683c9e113
Matija Turčec <turcec.matija@gmail.com>
2020-08-08 22:34:31 +0200

    added drawer functionality  to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,27 +1,54 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import NavMain from './components-small/Nav-main';
+import MenuIcon from '@material-ui/icons/Menu';
+import { Hidden, IconButton } from '@material-ui/core';
 
 function Navbar() {
+  const [open, setOpen] = useState<boolean>(false);
+  const [playDrawerAnimation, setPlayDrawerAnimation] = useState<boolean>(false);
+  const handleClick = () => {
+    setPlayDrawerAnimation( prev => !prev);
+  };
+  useEffect(() => {
+    if(!playDrawerAnimation)
+      setTimeout(function(){ setOpen( playDrawerAnimation ); }, 300);
+    else
+      setOpen( playDrawerAnimation );
+  }, [playDrawerAnimation]);
   return (
     <nav className="navbar">
       <h3 className="navbar__logo">
         Logo
       </h3>
-      <ul className="navbar__links">
-        <Link to="/">
-          <li>Home</li>
-        </Link>
-        <Link to="/about">
-          <li>About</li>
-        </Link>
-        <Link to="/portfolio">
-          <li>Portfolio</li>
-        </Link>
-      </ul>
-      <ul className="navbar__links">
-        <li>Facebook</li>
-        <li>Instagram</li>
-      </ul>
+
+      <Hidden xsDown>
+        <NavMain classProp="navbar__links" />
+        <ul className="navbar__links">
+          <li>Facebook</li>
+          <li>Instagram</li>
+        </ul>
+      </Hidden>
+
+      <Hidden smUp>
+        <IconButton onClick={handleClick} >
+          <MenuIcon />
+        </IconButton>
+      </Hidden>
+
+      <Hidden xsDown={!open} smUp>
+        <div className={"navbar__drawer " + (playDrawerAnimation ? "navbar__drawer-enter" : "navbar__drawer-exit" )} >
+          <div className="navbar">
+            <h3 className="navbar__logo">
+              Logo
+            </h3>
+            <IconButton onClick={handleClick} >
+              <MenuIcon />
+            </IconButton>
+
+          </div>
+          <NavMain classProp="navbar__links navbar__links-aside" setPlayDrawerAnimation={setPlayDrawerAnimation} />
+        </div>
+      </Hidden>
     </nav>
   );
 }
