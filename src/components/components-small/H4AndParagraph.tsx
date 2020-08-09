dddcdbb96d6cf49b3e63e5e77e1eacd4e9e75600
Matija Turčec <turcec.matija@gmail.com>
2020-08-09 14:30:19 +0200

    more basic stylings and stuff

--- src/components/components-small/H4AndParagraph.tsx
+++ src/components/components-small/H4AndParagraph.tsx
@@ -3,7 +3,7 @@ import { IH4AndP } from '../../interfaces';
 
 function H4AndParagraph( {header, paragraph}: IH4AndP ) {
   if(paragraph==="lorem")
-    paragraph = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt perferendis aliquid distinctio inventore officia eius, necessitatibus, excepturi asperiores voluptatum, atque dolorum. Repudiandae accusantium natus fugit cupiditate sed blanditiis rerum nostrum.Eveniet ducimus dolor, nihil assumenda illo, corporis natus quisquam magnam facilis debitis sit doloribus laudantium quam aliquid cupiditate obcaecati ipsam error! Quaerat doloribus sapiente laborum corrupti recusandae quae iste id?"
+    paragraph = "Lorem, ipsum dolor sit amet consectetur adipisicing elit."
     console.log("render")
   return (
     <>
--- src/components/components-small/WhatWeDo.tsx
+++ src/components/components-small/WhatWeDo.tsx
@@ -3,23 +3,25 @@ import H4AndParagraph from './H4AndParagraph';
 
 function WhatWeDo() {
   return (
-    <div className="info-container">
+    <div className="info__container-outer">
       <h2>What We Do</h2>
-      <div>
-        Image
-        <H4AndParagraph header="Test" paragraph="lorem" />
-      </div>
-      <div>
-        Image
-        <H4AndParagraph header="Test" paragraph="lorem" />
-      </div>
-      <div>
-        Image
-        <H4AndParagraph header="Test" paragraph="lorem" />
-      </div>
-      <div>
-        Image
-        <H4AndParagraph header="Test" paragraph="lorem" />
+      <div className="info__container">
+        <div className="info">
+          Image
+          <H4AndParagraph header="Test" paragraph="lorem" />
+        </div>
+        <div className="info">
+          Image
+          <H4AndParagraph header="Test" paragraph="lorem" />
+        </div>
+        <div className="info">
+          Image
+          <H4AndParagraph header="Test" paragraph="lorem" />
+        </div>
+        <div className="info">
+          Image
+          <H4AndParagraph header="Test" paragraph="lorem" />
+        </div>
       </div>
     </div>
   );
