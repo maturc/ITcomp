a28fc2f90031f12d12d64c4bd70984515f91b9e8
Matija Turčec <turcec.matija@gmail.com>
2020-08-10 18:15:54 +0200

    portfolio component 1.0

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -6,15 +6,17 @@ import nightSky from '../assets/night-sky.jpg'
 import photographer from '../assets/photographer.jpg'
 import road from '../assets/road-from-above.jpg'
 import snorkeling from '../assets/woman-snorkeling.jpg'
+import { Button } from '@material-ui/core';
 
 function Portfolio() {
   return (
     <div className="portfolio">
-      <img src={beach}        alt="project" />
-      <img src={forest}       alt="project" />
-      <img src={mountain}     alt="project" />
-      <img src={nightSky}     alt="project" />
-      <img src={photographer} alt="project" />
+      <a href="test" style={{backgroundImage: `url(${beach})`}}>{""}</a>
+      <a href="test" style={{backgroundImage: `url(${forest})`}}>{""}</a>
+      <a href="test" style={{backgroundImage: `url(${mountain})`}}>{""}</a>
+      <a href="test" style={{backgroundImage: `url(${nightSky})`}}>{""}</a>
+      <a href="test" style={{backgroundImage: `url(${photographer})`}}>{""}</a>
+      <div className="portfolio__button-container"><Button color="primary">More projects</Button></div>
     </div>
   );
 }
