e025066d270f33aadcec29f06c77b5bfc0ff10f9
Matija Turčec <turcec.matija@gmail.com>
2020-08-10 12:27:24 +0200

    stable?

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import beach from '../assets/beach.jpg'
+import forest from '../assets/forest.jpg'
+import mountain from '../assets/mountain.jpg'
+import nightSky from '../assets/night-sky.jpg'
+import photographer from '../assets/photographer.jpg'
+import road from '../assets/road-from-above.jpg'
+import snorkeling from '../assets/woman-snorkeling.jpg'
 
 function Portfolio() {
   return (
-    <section>Portfolio</section>
+    <div className="portfolio">
+      <img src={beach}        alt="project" />
+      <img src={forest}       alt="project" />
+      <img src={mountain}     alt="project" />
+      <img src={nightSky}     alt="project" />
+      <img src={photographer} alt="project" />
+    </div>
   );
 }
 
