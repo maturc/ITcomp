05e65879ff3f96ce388537d8ef01640b683e53bf
Matija Turčec <turcec.matija@gmail.com>
2020-08-10 18:14:25 +0200

    added portfolio to homepage

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from './components-small/Banner';
 import LearnMore from './components-small/LearnMore';
 import WhatWeDo from './components-small/WhatWeDo';
+import Portfolio from './Portfolio';
 
 function Home() {
   return (
@@ -11,12 +12,10 @@ function Home() {
         <LearnMore header="About Us" paragraph="lorem" button="Learn more" src="https://i.imgur.com/6beh1u3.jpg" alt="placeholder"/>
         <WhatWeDo />
       </div>
-      <section>
-        Portfolio
-      </section>
+      <Portfolio /  >
       <div className="container">
         <section>
-          Impressions
+          Testimonials
         </section>
         <section>
           Blog?
