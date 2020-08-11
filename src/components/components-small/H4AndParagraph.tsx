d71ffd833786e4edd55f190b9a6d030265a1c551
Matija Turčec <turcec.matija@gmail.com>
2020-08-11 23:48:19 +0200

    removed a console.log

--- src/components/components-small/H4AndParagraph.tsx
+++ src/components/components-small/H4AndParagraph.tsx
@@ -4,7 +4,6 @@ import { IH4AndP } from '../../interfaces';
 function H4AndParagraph( {header, paragraph}: IH4AndP ) {
   if(paragraph==="lorem")
     paragraph = "Lorem, ipsum dolor sit amet consectetur adipisicing elit."
-    console.log("render")
   return (
     <>
       <h4>{header}</h4>
