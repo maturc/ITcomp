91512878fbf959e4e8f88de91c48e3dc24ef5c90
Matija Turčec <turcec.matija@gmail.com>
2020-08-14 15:23:11 +0200

    updated what we do section, removed placeholders..

--- src/components/components-small/WhatWeDo.tsx
+++ src/components/components-small/WhatWeDo.tsx
@@ -1,26 +1,32 @@
 import React from 'react';
 import H4AndParagraph from './H4AndParagraph';
+import WebIcon from '@material-ui/icons/Web';
+import ComputerIcon from '@material-ui/icons/Computer';
+import ViewQuiltIcon from '@material-ui/icons/ViewQuilt';
+import SmartphoneIcon from '@material-ui/icons/Smartphone';
 
 function WhatWeDo() {
+  const iconSize = "4rem";
+  const iconColor = "#ff2b58"
   return (
     <div className="info__container-outer">
       <h2>What We Do</h2>
       <div className="info__container">
         <div className="info">
-          Image
-          <H4AndParagraph header="Test" paragraph="lorem" />
+          <WebIcon style={ {fontSize: `${iconSize}`, color: `${iconColor}`} } />
+          <H4AndParagraph header="Web design" paragraph="Our experienced design team can give shape to your vision. Whether you need a new design or if you want us to improve your existing design, we are confident that we will deliver." />
         </div>
         <div className="info">
-          Image
-          <H4AndParagraph header="Test" paragraph="lorem" />
+          <ComputerIcon style={ {fontSize: `${iconSize}`, color: `${iconColor}`} } />
+          <H4AndParagraph header="Web development" paragraph="Do you need a web application? A single page application, a content managment system, an application with server side rendering, or something else? No matter what your requirements are, we will bring your ideas to life." />
         </div>
         <div className="info">
-          Image
-          <H4AndParagraph header="Test" paragraph="lorem" />
+          <ViewQuiltIcon style={ {fontSize: `${iconSize}`, color: `${iconColor}`} } />
+          <H4AndParagraph header="UI/UX" paragraph="The user experience is one of the most important part of every application. It's all about convenience and pleasure of interacting with the application. We make sure that we bring the user experience to the next level." />
         </div>
         <div className="info">
-          Image
-          <H4AndParagraph header="Test" paragraph="lorem" />
+          <SmartphoneIcon style={ {fontSize: `${iconSize}`, color: `${iconColor}`} } />
+          <H4AndParagraph header="Mobile development" paragraph="Whether  you need a mobile application for your existing web application or a new one, our experienced team will make it happen." />
         </div>
       </div>
     </div>
