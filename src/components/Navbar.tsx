484bc6f5a89d3bf9e6e726c81cc4b3bb4368cd8c
Matija Turčec <turcec.matija@gmail.com>
2020-08-14 09:05:52 +0200

    added logo img

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import NavMain from './components-small/Nav-main';
 import MenuIcon from '@material-ui/icons/Menu';
 import { Hidden, IconButton } from '@material-ui/core';
+import logo from '../assets/logo.png'
 
 function Navbar() {
   const [open, setOpen] = useState<boolean>(false);
@@ -18,7 +19,7 @@ function Navbar() {
   return (
     <nav className="navbar">
       <h3 className="navbar__logo">
-        Logo
+        <img src={logo} alt="logo"/>
       </h3>
 
       <Hidden xsDown>
