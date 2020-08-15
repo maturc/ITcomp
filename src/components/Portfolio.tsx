2eafde73e9d0aeef09966fcae9efa2656b811293
Matija Turčec <turcec.matija@gmail.com>
2020-08-15 09:33:14 +0200

    added build images

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,22 +1,18 @@
 import React from 'react';
-import beach from '../assets/beach.jpg'
-import forest from '../assets/forest.jpg'
-import mountain from '../assets/mountain.jpg'
-import nightSky from '../assets/night-sky.jpg'
-import photographer from '../assets/photographer.jpg'
-import road from '../assets/road-from-above.jpg'
-import snorkeling from '../assets/woman-snorkeling.jpg'
+import graphs from '../assets/graphs.jpg'
+import mobileApp from '../assets/mobile-app.jpg'
+import server from '../assets/server.jpg'
+import galery from '../assets/galery.jpg'
+import chat from '../assets/chat.jpg'
 
 function Portfolio() {
   return (
     <div className="portfolio">
-      <a href="test" style={{backgroundImage: `url(${beach})`}}>{""}</a>
-      <a href="test" style={{backgroundImage: `url(${forest})`}}>{""}</a>
-      <a href="test" style={{backgroundImage: `url(${mountain})`}}>{""}</a>
-      <a href="test" style={{backgroundImage: `url(${nightSky})`}}>{""}</a>
-      <a href="test" style={{backgroundImage: `url(${photographer})`}}>{""}</a>
-      <a href="test" style={{backgroundImage: `url(${road})`}}>{""}</a>
-      <a href="test" style={{backgroundImage: `url(${snorkeling})`}}>{""}</a>
+      <a href="/portfolio/trafic" style={{backgroundImage: `url(${graphs})`}}>{""}</a>
+      <a href="/portfolio/mobile" style={{backgroundImage: `url(${mobileApp})`}}>{""}</a>
+      <a href="/portfolio/server" style={{backgroundImage: `url(${server})`}}>{""}</a>
+      <a href="/portfolio/galery" style={{backgroundImage: `url(${galery})`}}>{""}</a>
+      <a href="/portfolio/chat"   style={{backgroundImage: `url(${chat})`}}>{""}</a>
     </div>
   );
 }
--- src/components/PortfolioHome.tsx
+++ src/components/PortfolioHome.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
-import beach from '../assets/beach.jpg'
-import forest from '../assets/forest.jpg'
-import mountain from '../assets/mountain.jpg'
-import nightSky from '../assets/night-sky.jpg'
-import photographer from '../assets/photographer.jpg'
+import graphs from '../assets/graphs.jpg'
+import mobileApp from '../assets/mobile-app.jpg'
+import server from '../assets/server.jpg'
+import galery from '../assets/galery.jpg'
+import chat from '../assets/chat.jpg'
 import { Button } from '@material-ui/core';
+import buttonBackground from '../assets/portfolio-button-background.jpg'
 
 function PortfolioHome() {
   return (
     <div className="portfolio">
-      <a href="test" style={{backgroundImage: `url(${beach})`}}>{""}</a>
-      <a href="test" style={{backgroundImage: `url(${forest})`}}>{""}</a>
-      <a href="test" style={{backgroundImage: `url(${mountain})`}}>{""}</a>
-      <a href="test" style={{backgroundImage: `url(${nightSky})`}}>{""}</a>
-      <a href="test" style={{backgroundImage: `url(${photographer})`}}>{""}</a>
-      <div className="portfolio__button-container"><Button color="primary">More projects</Button></div>
+      <a href="/portfolio/trafic" style={{backgroundImage: `url(${graphs})`}}>{""}</a>
+      <a href="/portfolio/mobile" style={{backgroundImage: `url(${mobileApp})`}}>{""}</a>
+      <a href="/portfolio/server" style={{backgroundImage: `url(${server})`}}>{""}</a>
+      <a href="/portfolio/galery" style={{backgroundImage: `url(${galery})`}}>{""}</a>
+      <a href="/portfolio/chat"   style={{backgroundImage: `url(${chat})`}}>{""}</a>
+      <div className="portfolio__button-container" style={{backgroundImage: `url(${buttonBackground})`}}><Button color="secondary">More projects</Button></div>
     </div>
   );
 }
