d9bdb6299194eed5676b84f16a511412a942eab2
Matija Turčec <turcec.matija@gmail.com>
2020-08-15 09:32:40 +0200

    added logo and icons for fb and ig

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import NavMain from './components-small/Nav-main';
+import NavMain from './components-small/NavMain';
 import MenuIcon from '@material-ui/icons/Menu';
 import { Hidden, IconButton } from '@material-ui/core';
 import logo from '../assets/logo.png'
+import FacebookIcon from '@material-ui/icons/Facebook';
+import InstagramIcon from '@material-ui/icons/Instagram';
 
 function Navbar() {
   const [open, setOpen] = useState<boolean>(false);
@@ -23,10 +25,18 @@ function Navbar() {
       </h3>
 
       <Hidden xsDown>
-        <NavMain classProp="navbar__links" />
-        <ul className="navbar__links">
-          <li>Facebook</li>
-          <li>Instagram</li>
+        <NavMain classProp="navbar__links" showCurrentRoute={true} />
+        <ul className="navbar__links navbar__links-social">
+          <li>
+            <a href="https://www.facebook.com/" className="navbar__social">
+              <FacebookIcon />
+            </a>
+            </li>
+          <li>
+            <a href="https://www.instagram.com/" className="navbar__social">
+              <InstagramIcon />
+            </a>
+          </li>
         </ul>
       </Hidden>
 
@@ -40,14 +50,13 @@ function Navbar() {
         <div className={"navbar__drawer " + (playDrawerAnimation ? "navbar__drawer-enter" : "navbar__drawer-exit" )} >
           <div className="navbar">
             <h3 className="navbar__logo">
-              Logo
+              <img src={logo} alt="logo"/>
             </h3>
             <IconButton onClick={handleClick} >
               <MenuIcon />
             </IconButton>
-
           </div>
-          <NavMain classProp="navbar__links navbar__links-aside" setPlayDrawerAnimation={setPlayDrawerAnimation} />
+          <NavMain classProp="navbar__links navbar__links-aside" setPlayDrawerAnimation={setPlayDrawerAnimation} showCurrentRoute={true} />
         </div>
       </Hidden>
     </nav>
