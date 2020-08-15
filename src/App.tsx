1111d15706379370f8e0a3ee84ebfed86922f7ca
Matija Turčec <turcec.matija@gmail.com>
2020-08-15 17:28:00 +0200

    scrapped

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
 import Portfolio from './components/Portfolio';
-import Blog from './components/Blog';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import './App.css';
@@ -20,7 +19,6 @@ function App() {
           <Route path="/"          exact component={Home} />
           <Route path="/about"     exact component={About} />
           <Route path="/portfolio" exact component={Portfolio} />
-          <Route path="/blog"      exact component={Blog} />
           <Route path="/contact"   exact component={Contact} />
         </Switch>
         <Footer />
