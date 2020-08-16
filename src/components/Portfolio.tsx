d13a6c97c6854eaa5fefe16faa8bc236bab86fdf
Matija Turčec <turcec.matija@gmail.com>
2020-08-16 23:01:53 +0200

    finished and expanded portfolio main page

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,19 +1,30 @@
 import React from 'react';
-import graphs from '../assets/graphs.jpg'
-import mobileApp from '../assets/mobile-app.jpg'
-import server from '../assets/server.jpg'
-import galery from '../assets/galery.jpg'
-import chat from '../assets/chat.jpg'
+import graphs from '../assets/graphs.jpg';
+import mobileApp from '../assets/mobile-app.jpg';
+import server from '../assets/server.jpg';
+import galery from '../assets/galery.jpg';
+import chat from '../assets/chat.jpg';
+import portfolio from '../assets/portfolio-half.jpg';
+import news from '../assets/news.jpg';
+import banking from '../assets/banking.jpg';
+import eComerce from '../assets/e-comerce.jpg';
+import BannerHalf from './components-small/BannerHalf';
 
 function Portfolio() {
   return (
-    <div className="portfolio">
-      <a href="/portfolio/trafic" style={{backgroundImage: `url(${graphs})`   } }>{""}</a>
-      <a href="/portfolio/mobile" style={{backgroundImage: `url(${mobileApp})`} }>{""}</a>
-      <a href="/portfolio/server" style={{backgroundImage: `url(${server})`   } }>{""}</a>
-      <a href="/portfolio/galery" style={{backgroundImage: `url(${galery})`   } }>{""}</a>
-      <a href="/portfolio/chat"   style={{backgroundImage: `url(${chat})`     } }>{""}</a>
-    </div>
+    <>
+      <BannerHalf bannerText="Portfolio" bannerUrl={portfolio} />
+      <div className="portfolio portfolio__main">
+        <a href="/portfolio/traffic"   style={{backgroundImage: `url(${graphs})`   } }>{""}</a>
+        <a href="/portfolio/mobile"    style={{backgroundImage: `url(${mobileApp})`} }>{""}</a>
+        <a href="/portfolio/server"    style={{backgroundImage: `url(${server})`   } }>{""}</a>
+        <a href="/portfolio/galery"    style={{backgroundImage: `url(${galery})`   } }>{""}</a>
+        <a href="/portfolio/chat"      style={{backgroundImage: `url(${chat})`     } }>{""}</a>
+        <a href="/portfolio/news"      style={{backgroundImage: `url(${news})`     } }>{""}</a>
+        <a href="/portfolio/banking"   style={{backgroundImage: `url(${banking})`  } }>{""}</a>
+        <a href="/portfolio/e-comerce" style={{backgroundImage: `url(${eComerce})` } }>{""}</a>
+      </div>
+    </>
   );
 }
 
