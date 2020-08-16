d0b3fc5d3ab51a8d3acee6ebc5bd2c755238871c
Matija Turčec <turcec.matija@gmail.com>
2020-08-16 23:02:38 +0200

    removed an old console log

--- src/components/components-small/LearnMore.tsx
+++ src/components/components-small/LearnMore.tsx
@@ -6,7 +6,6 @@ import { Link } from 'react-router-dom';
 function LearnMore( {header, paragraph, button, src, alt}: ILearnMore ) {
   if(paragraph==="lorem")
     paragraph = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt perferendis aliquid distinctio inventore officia eius, necessitatibus, excepturi asperiores voluptatum, atque dolorum. Repudiandae accusantium natus fugit cupiditate sed blanditiis rerum nostrum.Eveniet ducimus dolor, nihil assumenda illo, corporis natus quisquam magnam facilis debitis sit doloribus laudantium quam aliquid cupiditate obcaecati ipsam error! Quaerat doloribus sapiente laborum corrupti recusandae quae iste id?"
-    console.log("render")
   return (
     <div className="learn-more">
       <div className="learn-more__text">
