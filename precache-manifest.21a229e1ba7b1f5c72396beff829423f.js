40a64bb518e104daee6eefdf76c3a15ae14efd5a
Matija Turčec <turcec.matija@gmail.com>
2020-08-17 23:31:23 +0200

    Updates

---
+++ precache-manifest.21a229e1ba7b1f5c72396beff829423f.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5b1b5d65acbfea40e7e6fe458e111a6",
+    "url": "/ITcomp/index.html"
+  },
+  {
+    "revision": "adbd0262f464445af9c5",
+    "url": "/ITcomp/static/css/main.d9f39aa9.chunk.css"
+  },
+  {
+    "revision": "9691e5b067697a05a35c",
+    "url": "/ITcomp/static/js/2.ef3da4ca.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ITcomp/static/js/2.ef3da4ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adbd0262f464445af9c5",
+    "url": "/ITcomp/static/js/main.027c991b.chunk.js"
+  },
+  {
+    "revision": "0d25438d75b400a1c45b",
+    "url": "/ITcomp/static/js/runtime-main.80787e56.js"
+  },
+  {
+    "revision": "f23bcd9426e117c4c14f7149193bf655",
+    "url": "/ITcomp/static/media/about-us-half.f23bcd94.jpg"
+  },
+  {
+    "revision": "4c3cadef78b0030a1d3843f5ddca9fb5",
+    "url": "/ITcomp/static/media/about-us.4c3cadef.jpg"
+  },
+  {
+    "revision": "214fb2c0e54e0ea4566c2594c1d3fb45",
+    "url": "/ITcomp/static/media/awesome.214fb2c0.jpg"
+  },
+  {
+    "revision": "0271289ddc216679672e1000b0c7bf87",
+    "url": "/ITcomp/static/media/banking.0271289d.jpg"
+  },
+  {
+    "revision": "3b918de4c37b4c6d9680fff6af737324",
+    "url": "/ITcomp/static/media/banner.3b918de4.jpg"
+  },
+  {
+    "revision": "84a9f18f201b0fb66033149af0a377c9",
+    "url": "/ITcomp/static/media/chat.84a9f18f.jpg"
+  },
+  {
+    "revision": "54d68182c1b5a90862f92709ff33a892",
+    "url": "/ITcomp/static/media/contact-half.54d68182.jpg"
+  },
+  {
+    "revision": "f90d6f36204172a5c183a0edfa8f7ddd",
+    "url": "/ITcomp/static/media/e-comerce.f90d6f36.jpg"
+  },
+  {
+    "revision": "b45937627b2310219e8ea7438f21606c",
+    "url": "/ITcomp/static/media/galery.b4593762.jpg"
+  },
+  {
+    "revision": "d0653681f370077f6014ed94e5ee84ac",
+    "url": "/ITcomp/static/media/graphs.d0653681.jpg"
+  },
+  {
+    "revision": "fc148e4aa4a44699dffcd8eb4f7f5178",
+    "url": "/ITcomp/static/media/group.fc148e4a.jpg"
+  },
+  {
+    "revision": "e211b73f8b53eded913d76799660200f",
+    "url": "/ITcomp/static/media/ideas.e211b73f.jpg"
+  },
+  {
+    "revision": "9afc6077b211fd4e22106d8ce77b9f1b",
+    "url": "/ITcomp/static/media/iva.9afc6077.jpg"
+  },
+  {
+    "revision": "e72f982bac502c7ce67519de7f3ef9bf",
+    "url": "/ITcomp/static/media/ivan.e72f982b.jpg"
+  },
+  {
+    "revision": "a557c0b760510ae877c04e49a3375957",
+    "url": "/ITcomp/static/media/josip.a557c0b7.jpg"
+  },
+  {
+    "revision": "e1e462b1e551e1765b1905363d7f1ea3",
+    "url": "/ITcomp/static/media/julia.e1e462b1.jpg"
+  },
+  {
+    "revision": "eec0c6b1c6712c3c0f5cdccdae942c2b",
+    "url": "/ITcomp/static/media/lori.eec0c6b1.jpg"
+  },
+  {
+    "revision": "663c2042b6a46dcbe7b57a64d3b9f637",
+    "url": "/ITcomp/static/media/marko.663c2042.jpg"
+  },
+  {
+    "revision": "2b3a6a13c603bc1e56570bbf6f57753e",
+    "url": "/ITcomp/static/media/meeting.2b3a6a13.jpg"
+  },
+  {
+    "revision": "64a52658d2ceaddc1e572fd945a2550c",
+    "url": "/ITcomp/static/media/mobile-app.64a52658.jpg"
+  },
+  {
+    "revision": "4f3a41c938805f84b70725442ae60a95",
+    "url": "/ITcomp/static/media/news.4f3a41c9.jpg"
+  },
+  {
+    "revision": "76d123c96888d788801005597e2e61ea",
+    "url": "/ITcomp/static/media/nikolina.76d123c9.jpg"
+  },
+  {
+    "revision": "a1dd157b67892a67c61b4fa8355f3b7a",
+    "url": "/ITcomp/static/media/portfolio-button-background.a1dd157b.jpg"
+  },
+  {
+    "revision": "134754b62a6de2e305672c58495aee39",
+    "url": "/ITcomp/static/media/portfolio-half.134754b6.jpg"
+  },
+  {
+    "revision": "4d62d498bfb25f1209ed8e9564a06b66",
+    "url": "/ITcomp/static/media/portfolio-template-half.4d62d498.jpg"
+  },
+  {
+    "revision": "ba74583974466b954dd54ffbae425f31",
+    "url": "/ITcomp/static/media/server.ba745839.jpg"
+  },
+  {
+    "revision": "9fb9031d92c290988d89456065b994b6",
+    "url": "/ITcomp/static/media/tomislav.9fb9031d.jpg"
+  }
+]);
